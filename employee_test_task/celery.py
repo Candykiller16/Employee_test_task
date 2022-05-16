490741f2b132743cb5c2c1d63bf368c180b62815
Candykiller16 <candykiller16@mail.ru>
2022-05-16 23:40:19 +0300

    finish

--- employee_test_task/celery.py
+++ employee_test_task/celery.py
@@ -1,8 +1,16 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'employee_test_task.settings')
 
 celery_app = Celery('employee_test_task')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
+
+celery_app.conf.beat_schedule = {
+    'add-salary-every-2-hours': {
+        'task': 'employees.tasks.send_money',
+        'schedule': crontab(minute=0, hour='*/2'),
+    },
+}
--- employees/tasks.py
+++ employees/tasks.py
@@ -3,11 +3,6 @@ from .models import EmployeeMPTT
 
 
 @shared_task
-def adding_task(x, y):
-    return x + y
-
-
-@shared_task
 def send_money():
     for i in EmployeeMPTT.objects.all():
         i.add_salary()
